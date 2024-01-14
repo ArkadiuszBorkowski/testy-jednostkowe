fe533a8ecb6e625e8bdad52d3849ded6d3c4b91e
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2024-01-14 23:40:19 +0100

    Testy jednostkowe w Java. Konfiguracja Maven, dodanie frameworka JUnit 5, konfiguracja pom.xml

---
+++ src/test/java/pl/javastart/testowanie/CalculatorTest.java
@@ -0,0 +1,13 @@
+package pl.javastart.testowanie;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    public void shouldTestSth() {
+
+    }
+}
